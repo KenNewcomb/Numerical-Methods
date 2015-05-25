8229a8eb31aa1d6abdc39b2209b4c79d5768c94e
Ken Newcomb <KenNewcombNJ@gmail.com>
2015-05-25 21:59:13 +0000

    Functioning Taylor Series ODE solver

--- Taylor_ODE/taylor.py
+++ Taylor_ODE/taylor.py
@@ -8,22 +8,24 @@ import numpy
 ## y''' = y''
 
 # inputs
-dx = 0.01
+dx = 0.1
 init_x = 0.0
-init_y = 1.0
+init_y = 2.0
 final_x = 4.0
 
-for i in numpy.linspace(init_x + dx, final_x, (final_x-init_x)/dx):
-	if i == init_x+dx:
-		final_y = init_y
-		prev_y = init_y
-	else:
-		final_y += prev_y
-	
-	# Add derivatives
-	final_y += (prev_y - i)*dx/(math.factorial(1)) + (prev_y - i - 1)*dx**2/(math.factorial(2)) + (prev_y - i -1)*dx**3/(math.factorial(3))
-	print("y({0}) = {1}".format(init_x + i, final_y))
+# outputs
+final_y = init_y
+prev_y = init_y
+
+for i in numpy.linspace(init_x, final_x-dx, (final_x-init_x)/dx):
+	final_y = prev_y + (prev_y - i)*dx/(math.factorial(1)) + (prev_y - i - 1)*(dx**2)/(math.factorial(2)) + (prev_y - i -1)*(dx**3)/(math.factorial(3))
+	prev_y = final_y
+
+print("Results:\n")
 print("y({0}) = {1}".format(final_x, final_y))
 
 # Analytical solution: y(x) = e^x + x + 1
-print("Exact solution = {0}".format(math.e**final_x + final_x + 1))
+exact = math.e**final_x + final_x + 1
+
+print("Exact solution = {0}".format(exact))
+print("Relative error = {0}%".format(100*(final_y-exact)/exact))
